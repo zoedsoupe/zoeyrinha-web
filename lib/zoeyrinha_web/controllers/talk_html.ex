d25aa627f0ebe03714ecda16fe7fef42f501fbf4
Zoey Pessanha <zoey.spessanha@zeetech.io>
2026-09-05 13:18:21 -0300

    chore: rounded talk photos too

--- lib/zoeyrinha_web/controllers/talk_html.ex
+++ lib/zoeyrinha_web/controllers/talk_html.ex
@@ -83,7 +83,7 @@ defmodule ZoeyrinhaWeb.TalkHTML do
                 :if={talk.image}
                 src={talk.image}
                 alt=""
-                class="w-36 h-36 sm:w-42 sm:h-42 object-cover rounded border border-selection shrink-0"
+                class="w-36 h-36 sm:w-42 sm:h-42 object-cover rounded-full border border-selection shrink-0"
               />
             </div>
           </li>
